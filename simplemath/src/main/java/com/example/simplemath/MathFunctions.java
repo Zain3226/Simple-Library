bacb0805d975c07196ddf603b72e4029b78cb126
Zain3226 <33096735+Zain3226@users.noreply.github.com>
2020-02-27 14:54:51 +0500

    Initial commit

---
+++ simplemath/src/main/java/com/example/simplemath/MathFunctions.java
@@ -0,0 +1,25 @@
+package com.example.simplemath;
+
+public class MathFunctions {
+
+    public static int Plus(int a, int b){
+        return a+b;
+    }
+
+    public static int Subtract(int a, int b){
+        return a-b;
+    }
+
+    public static int Multiple(int a, int b){
+        return a*b;
+    }
+
+    public static float Division(int a, int b){
+        if(b==0){
+            throw new IllegalArgumentException("Divide by zero Error");
+        }
+        else {
+            return a/b;
+        }
+    }
+}
